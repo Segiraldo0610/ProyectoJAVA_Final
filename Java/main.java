5c3c8ff10742e76714fc01004f3d9cd6a285c2de
Samuel Enrique Giraldo Sabogal <giraldosabogalsamuel@gmail.com>
2024-10-21 21:58:28 -0500

    Update main.java

--- Java/main.java
+++ Java/main.java
@@ -1,1 +1,8 @@
+import controllers.VentasController;
 
+public class Main {
+    public static void main(String[] args) {
+        VentasController controller = new VentasController();
+        controller.iniciar();
+    }
+}
