2b8cc7904c09c5d979b77c0cbdbc63b7f7689c40
Samuel Enrique Giraldo Sabogal <giraldosabogalsamuel@gmail.com>
2024-10-21 21:03:29 -0500

    Update Producto.java

--- Java/src/models/Producto.java
+++ Java/src/models/Producto.java
@@ -1,1 +1,31 @@
+public class Producto{
+    private String nombre;
+    private double precioBase;
+    private int cantidadDisponible;
 
+    public Producto(String nombre, double precioBase, int cantidadDisponible){
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public double getPrecioBase(){
+        return precioBase;
+    }
+    public int getCantidadDisponible(){
+        return cantidadDisponible;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setPrecioBase(double precioBase){
+        this.precioBase = precioBase;
+    }
+    public void setCantidadDisponible(int cantidadDisponible){
+        this.cantidadDisponible = cantidadDisponible;
+    }
+}
