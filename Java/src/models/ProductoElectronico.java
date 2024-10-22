e601fe6cf1f449e7c874d7431174d8a9f8a832d9
Samuel Enrique Giraldo Sabogal <giraldosabogalsamuel@gmail.com>
2024-10-21 21:03:53 -0500

    Update ProductoElectronico.java

--- Java/src/models/ProductoElectronico.java
+++ Java/src/models/ProductoElectronico.java
@@ -1,1 +1,17 @@
+public class ProductoElectronico extends Producto { // Herencia
+    private int garantia;
+
+    public ProductoElectronico(String nombre, double precioBase, int cantidadDisponible, int garantia) {
+        super(nombre, precioBase, cantidadDisponible);
+        this.garantia = garantia;
+    }
+
+    public int getGarantia() {
+        return garantia;
+    }
+    
+    public void setGarantia(int garantia) {
+        this.garantia = garantia;
+    }
+}
 
