81963651b2fa7e420a898a32d15509a51e5dc141
Samuel Enrique Giraldo Sabogal <giraldosabogalsamuel@gmail.com>
2024-10-21 21:04:11 -0500

    Update ProductoAlimenticio.java

--- Java/src/models/ProductoAlimenticio.java
+++ Java/src/models/ProductoAlimenticio.java
@@ -1,1 +1,14 @@
+public class ProductoAlimenticio extends Producto{ //Herencia
+    private String fechaCaducidad;
 
+    public ProductoAlimenticio(String nombre, double precioBase, int cantidadDisponible, String fechaCaducidad) {
+        super(nombre, precioBase, cantidadDisponible);
+        this.fechaCaducidad = fechaCaducidad;
+    }
+    public String getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+    public void setFechaCaducidad(String fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+}
